ec869417185538c3e38f96e6787e4f5dca45bb19
SethEvry <92412142+SethEvry@users.noreply.github.com>
2022-01-12 03:36:37 -0500

    edit header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import './footer.css';
 
 export default function Footer() {
+    const [currentDate, setCurrentDate] = useState();
+
+    useEffect(()=> {
+        const date = new Date()
+        const options = {
+            weekday: 'short',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric'
+        }
+        setCurrentDate(date.toLocaleDateString('en-US', options));
+    }, [currentDate])
+
     return (
         <div className="footer">
-        Footer
+        <h4>Current Date: {currentDate}</h4>
         </div>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
-import './header.css';
+import "./header.css";
+
+//react-icons
+import { ImArrowLeft, ImArrowRight } from "react-icons/im";
+import { IconContext } from "react-icons/lib";
 
 export default function Header() {
-    
-    return (
-        <div className="header" >
-            Header
-        </div>
-    )
+  return (
+    <div className="header">
+      <IconContext.Provider value={{className: 'header_left', size: '1.5em'}}>
+        <ImArrowLeft />
+      </IconContext.Provider>
+      <h3 className="header_title">Seth's Week</h3>
+      <IconContext.Provider value={{className: 'header_right', size: '1.5em'}}>
+        <ImArrowRight />
+      </IconContext.Provider>
+    </div>
+  );
 }
