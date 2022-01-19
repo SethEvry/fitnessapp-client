23e4f4ba5103cf3b18ec737d926f45a83a334241
SethEvry <92412142+SethEvry@users.noreply.github.com>
2022-01-19 06:29:13 -0500

    add back button

--- src/components/AddWorkout/AddWorkout.js
+++ src/components/AddWorkout/AddWorkout.js
@@ -1,10 +1,19 @@
 import { useContext, useState } from "react";
-import { Modal } from "..";
-import { ModalContext } from "../../context/ModalContext";
-import { WorkoutContext } from "../../context/WorkoutContext";
-import { Title, SetsnReps, Description } from "./subcomponents";
 import { uid } from "uid";
 
+//context
+import { ModalContext } from "../../context/ModalContext";
+import { WorkoutContext } from "../../context/WorkoutContext";
+
+//components
+import { Modal } from "..";
+import { Title, SetsnReps, Description } from "./subcomponents";
+
+
+//react-icons
+import { ImArrowLeft } from "react-icons/im";
+import { IconContext } from "react-icons/lib";
+//
 export default function AddWorkout({ date }) {
   const [step, setStep] = useState(0);
   const [title, setTitle] = useState("");
@@ -27,6 +36,13 @@ export default function AddWorkout({ date }) {
 
   return (
     <Modal>
+      {step > 0 ? (
+        <IconContext.Provider
+          value={{ className: "header_left", size: "1.5em" }}
+        >
+          <ImArrowLeft onClick={() => setStep((oldStep) => oldStep - 1)} />
+        </IconContext.Provider>
+      ) : null}
       {
         [
           <Title title={title} setTitle={setTitle} setStep={setStep} />,
